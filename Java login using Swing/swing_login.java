fd3179c271f21244c7510892b99f5bb4d1f5225f
kvardhan10 <kanishkvardhanan@gmail.com>
2022-02-21 13:06:09 +0530

    get button source for multiple listeners

--- Java login using Swing/swing_login.java
+++ Java login using Swing/swing_login.java
@@ -1,25 +1,24 @@
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
-import java.util.*;
+import java.sql.*;
 
-public class swing_login implements ActionListener {
-  private static JLabel password1, label;
+public class swing_login implements ActionListener{
+  private static JLabel password1, label, home;
 	private static JTextField username;
-	private static JButton button;
+	private static JButton button, button1;
 	private static JPasswordField Password;
-  public static void main(String args[]){
-  System.out.println("Hello World");
 
+  public static void main(String args[]){
 
   JPanel panel = new JPanel();
   panel.setLayout(null);
 
   JFrame frame = new JFrame();
   frame.setTitle("LOGIN PAGE");
-  frame.setLocation(new Point(500, 300));
+  frame.setLocation(new Point(500, 200)); // frame position wrt your screen. So, 500px from left and 200px from top
   frame.add(panel);
-  frame.setSize(new Dimension(400, 200));
+  frame.setSize(new Dimension(800, 400));
   frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
   label = new JLabel("Username");
@@ -30,21 +29,30 @@ public class swing_login implements ActionListener {
   username.setBounds(100, 27, 193, 28);
   panel.add(username);
 
-password1 = new JLabel("Password");
-password1.setBounds(100, 55, 70, 20);
-panel.add(password1);
+  password1 = new JLabel("Password");
+  password1.setBounds(100, 55, 70, 20);
+  panel.add(password1);
 
-Password = new JPasswordField();
-Password.setBounds(100, 75, 193, 28);
-panel.add(Password);
+  Password = new JPasswordField();
+  Password.setBounds(100, 75, 193, 28);
+  panel.add(Password);
 
-button = new JButton("Login");
-button.setBounds(100, 110, 90, 25);
-button.setForeground(Color.WHITE);
-button.setBackground(Color.BLACK);
-button.addActionListener((ActionListener) new swing_login());
-panel.add(button);
+  button = new JButton("Login");
+  button.setBounds(100, 110, 90, 25);
+  button.setForeground(Color.WHITE);
+  button.setBackground(Color.BLACK);
+  button.addActionListener((ActionListener) new swing_login());
+  panel.add(button);
 
+  button1 = new JButton("Register");
+  button1.setBounds(200, 110, 90, 25);
+  button1.setForeground(Color.WHITE);
+  button1.setBackground(Color.BLACK);
+  button1.addActionListener((ActionListener) new swing_login());
+  panel.add(button1);
+
+  // frame.show();
+  frame.setVisible(true);
   }
 
   @Override
@@ -52,10 +60,12 @@ panel.add(button);
 	String Username = username.getText();
 	String Password1 = Password.getText();
 
-	if (Username.equals("section.io") && Password1.equals("123"))
-		JOptionPane.showMessageDialog(null, "Login Successful");
-	else
-		JOptionPane.showMessageDialog(null, "Username or Password mismatch ");
+  if(e.getSource() == button){
+    JOptionPane.showMessageDialog(null, "Login Successful");
+  }
+  else{
+    JOptionPane.showMessageDialog(null, "Registration Successful");
   }
 
-}
+} //end actionPerformed
+} //end swing_login
