527b2ce518a756d5e764ab7b1ba02cbb4189bb47
kvardhan10 <kanishkvardhanan@gmail.com>
2022-03-17 01:28:54 +0530

    galaxy uploaded

---
+++ 3js/Galaxy/script.js
@@ -0,0 +1,82 @@
+var scene = new THREE.Scene();
+
+var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 1000);
+camera.position.z = 250; //OG: 400
+// camera.position.y = 200;
+// camera.position.x = 0;
+
+var renderer = new THREE.WebGLRenderer({antialias: true});
+renderer.setClearColor('#000000');
+renderer.setSize(window.innerWidth, window.innerHeight);
+
+document.getElementById('canvas').appendChild(renderer.domElement);
+
+circle = new THREE.Object3D();
+skelet = new THREE.Object3D();
+particle = new THREE.Object3D();
+
+scene.add(circle);
+scene.add(skelet);
+scene.add(particle);
+
+const stars = new THREE.OctahedronGeometry( .5, 2 );
+const material = new THREE.MeshBasicMaterial( { color: 0xffffff } );
+
+for (var i = 0; i < 10000; i++) {
+		var mesh = new THREE.Mesh( stars, material );
+		mesh.position.set(Math.random()-.5, Math.random()-.5, Math.random()-.5);
+    mesh.position.multiplyScalar(100 + (Math.random() * 2000));
+		//expands stars as much as possible
+		particle.add(mesh)
+	}
+
+//common material
+var mat = new THREE.MeshStandardMaterial({});
+
+var mat2 = new THREE.MeshStandardMaterial({color: 0xffffff, wireframe: true });
+
+var innerCircle = new THREE.Mesh(new THREE.SphereGeometry( 80, 32, 32 ), mat)
+circle.add(innerCircle)
+var outerCircle = new THREE.Mesh(new THREE.OctahedronGeometry( 10, 2 ), mat2)
+outerCircle.scale.x = outerCircle.scale.y = outerCircle.scale.z = 13;
+skelet.add(outerCircle)
+
+var lights = [];
+lights[0] = new THREE.HemisphereLight( 0x11E8BB, 1 );
+lights[0].position.set( -1, 1, 0 );
+lights[1] = new THREE.HemisphereLight( 0x2200a9, 0.5 );
+lights[1].position.set( 1, -1, 0 );
+scene.add( lights[0] );
+scene.add( lights[1] );
+
+window.addEventListener('resize', onWindowResize, false);
+
+function onWindowResize() {
+camera.aspect = window.innerWidth / window.innerHeight;
+camera.updateProjectionMatrix();
+renderer.setSize(window.innerWidth, window.innerHeight);
+}
+
+window.addEventListener('mousemove', onDocumentMouseMove, false);
+
+function onDocumentMouseMove(e) {
+		mouseX = e.clientX - (window.innerWidth / 2);
+		mouseY = e.clientY - (window.innerHeight / 2);
+}
+
+var mouseX = 0
+var mouseY = 0
+
+function animate() {
+	requestAnimationFrame( animate );
+	particle.position.x += (mouseX - particle.position.x) * 0.0003;
+	particle.position.y += (-mouseY - particle.position.y) * 0.0003;
+	// moves stars, but out of the screen
+	particle.rotation.y += .001;
+	circle.rotation.x -= 0.0020;
+  circle.rotation.y += 0.0030;
+  skelet.rotation.x += 0.0010;
+  skelet.rotation.y -= 0.0020;
+	renderer.render( scene, camera );
+}
+animate();
--- 3js/build_files/stars_moving_at_you.js
+++ 3js/build_files/stars_moving_at_you.js
@@ -1,9 +1,9 @@
 var scene = new THREE.Scene();
 
 var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 1000);
-camera.position.z = 400; //OG: 400
-camera.position.y = 200;
-camera.position.x = 200;
+camera.position.z = 300; //OG: 400
+// camera.position.y = 200;
+// camera.position.x = 200;
 
 var renderer = new THREE.WebGLRenderer({antialias: true});
 renderer.setClearColor('#000000');
@@ -20,9 +20,9 @@ const material = new THREE.MeshBasicMaterial( { color: 0xffffff } );
 
 for (var i = 0; i < 10000; i++) {
 		var mesh = new THREE.Mesh( stars, material );
-		mesh.position.x = ((Math.random()) * 500);
-	  mesh.position.y = ((Math.random()) * 500);
-	  mesh.position.z = ((Math.random()) * 500);
+		mesh.position.set(Math.random()-.5, Math.random()-.5, Math.random()-.5);
+    mesh.position.multiplyScalar(90 + (Math.random() * 1000));
+		//expands the dots collected in cube
 		particle.add(mesh)
 	}
 
